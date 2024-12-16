ab46eb456d653aec228790d724c9ef9c656055ed
Jenrola Jackson <jacksinsthegenius@gmail.com>
2024-12-16 20:10:03 +0100

    test deploy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Alter the majority</h1>
+    <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eius et nihil numquam esse, eligendi eos cumque. Cum
         ipsam cupiditate, omnis beatae quaerat animi possimus minima doloremque, asperiores inventore culpa!</p>
 </template>
