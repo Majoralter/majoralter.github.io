4e242f45da776f925d97bdc71cae0436ddcf9bb1
Jenrola Jackson <jacksinsthegenius@gmail.com>
2024-12-16 20:06:06 +0100

    update vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('@vue/cli-service')
+
+// vue.config.js
+module.exports = defineConfig({
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/main.scss";`,
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugin("define").tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_OPTIONS_API__: "true",
+        __VUE_PROD_DEVTOOLS__: "false",
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "true",
+      });
+      return definitions;
+    });
+  }
+});
