ed8f137037b022c4c9e43275da187af4f7f154f5
Jenrola Jackson <jacksinsthegenius@gmail.com>
2025-01-30 18:17:45 +0100

    Style and format .md content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { remarkModifiedTime } from './src/remark-modified-time.mjs';
+import { remarkReadingTime } from './src/remark-reading-time.mjs';
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkModifiedTime, remarkReadingTime],
+    shikiConfig: {
+      theme: "kanagawa-dragon",
+      defaultColor: false,
+    },
+  },
   integrations: [icon()],
+  output: "server",
 });
