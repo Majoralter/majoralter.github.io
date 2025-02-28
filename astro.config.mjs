523e2d805b63928c068dda611aca68682e97e319
Jenrola Jackson <jacksinsthegenius@gmail.com>
2025-02-28 23:23:41 +0100

    Finish Nav component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://majoralter.github.io",
-  base: "https://github.com/Majoralter/majoralter.github.io",
+  // base: "https://github.com/Majoralter/majoralter.github.io",
 });
