5678dee07713481d1a22810aa233a25fedf7a41d
Jenrola Jackson <jacksinsthegenius@gmail.com>
2025-03-02 20:33:58 +0100

    Add reading time feature and update blog content handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+import { remarkReadingTime } from './src/remark-reading-time.mjs';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://majoralter.github.io",
+
   // base: "https://github.com/Majoralter/majoralter.github.io",
   markdown: {
     shikiConfig: {
       theme: 'vesper'
-    }
-  }
+    },
+    remarkPlugins: [remarkReadingTime],
+  },
+
+  integrations: [mdx()]
 });
