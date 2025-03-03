25327ab5923e5bfb21b820c6f2a21c67826582af
Jenrola Jackson <jacksinsthegenius@gmail.com>
2025-03-03 13:36:57 +0100

    Optimize image loading for better ux and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
-import { remarkReadingTime } from './src/remark-reading-time.mjs';
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
+import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://majoralter.github.io",
-
-  // base: "https://github.com/Majoralter/majoralter.github.io",
   markdown: {
     shikiConfig: {
-      theme: 'vesper'
+      theme: "vesper",
     },
     remarkPlugins: [remarkReadingTime],
   },
-
-  integrations: [mdx()]
+  integrations: [mdx()],
+  image: {
+    service: imageService({
+      placeholder: "lqip",
+    }),
+  },
 });
