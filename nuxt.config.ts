4b1ef61144c3b8de71ca6c09e0e10fff3ad7f226
Majoralter <jacksinsthegenius@gmail.com>
2025-08-31 15:18:39 +0100

    Minor updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  ssr: false,
-  nitro: {
-    preset: "vercel",
-  },
+  ssr: true,
 
   app: {
     pageTransition: { name: "page", mode: "out-in" },
+    baseURL: "/majoralter.github.io/",
+    buildAssetsDir: "assets",
 
     head: {
       htmlAttrs: {
